6368d7708a40742d8a522d0c0280ceada21868e9
oscarbego <oscar.bego@gmail.com>
2017-10-27 12:49:43 -0600

    save

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ var primerEventoDia = undefined;
 
 var rule = new schedule.RecurrenceRule();
   rule.dayOfWeek = [0, new schedule.Range(0, 6)];
-  rule.hour = 12; //rule.hour = 9;
+  rule.hour = 14; //rule.hour = 9;
   rule.minute = 35;
 
 var j = schedule.scheduleJob(rule, function () {
