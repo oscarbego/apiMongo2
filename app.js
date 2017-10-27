c99f05d282bc0b94fd9b699228e9fd6cc04609b2
oscarbego <oscar.bego@gmail.com>
2017-10-27 13:33:00 -0600

    save

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ var primerEventoDia = undefined;
 var rule = new schedule.RecurrenceRule();
   rule.dayOfWeek = [0, new schedule.Range(0, 6)];
   rule.hour = 13; //rule.hour = 9;
-  rule.minute = 18;
+  rule.minute = 34;
 
 var j = schedule.scheduleJob(rule, function () {
   console.log('Alarma ');
@@ -60,11 +60,16 @@ var j = schedule.scheduleJob(rule, function () {
   console.log(".-.-.-.-.-.-.-.");
   
 
+  var d = new Date();
+  d.setHours(d.getHours() + 2);
+
+  
+
   var entry = {
 		imei:    862462035861144,
 		iniKms:  primerEventoDia.data[11].value, //primerEventoDia.data[11].value,
 		finKms:  eventos[eventos.length - 1].data[11].value,
-		fecha:   new Date().addHours(2),
+		fecha:   d,
 		kmsRecorridos: eventos[eventos.length - 1].data[11].value - primerEventoDia.data[11].value
 	};
 
