c2f6ca5093a2651effe25fdefd63c9863fa4bc93
oscarbego <oscar.bego@gmail.com>
2017-10-31 10:54:51 -0700

    save

--- controllers/carsKms.js
+++ controllers/carsKms.js
@@ -89,18 +89,17 @@ exports.addKmsLocalEntry = function(entry) {
 
 //PUT - Update a register already exists
 exports.updateKmsEntry = function(req, res) {
-	TVShow.findById(req.params.id, function(err, tvshow) {
-		tvshow.title   = req.body.petId;
-		tvshow.year    = req.body.year;
-		tvshow.country = req.body.country;
-		tvshow.poster  = req.body.poster;
-		tvshow.seasons = req.body.seasons;
-		tvshow.genre   = req.body.genre;
-		tvshow.summary = req.body.summary;
-
-		tvshow.save(function(err) {
+	CarsKms.findById(req.params.id, function(err, entry) {
+		
+		entry.imei =    req.body.imei,
+		entry.iniKms =  req.body.iniKms,
+		entry.finKms =  req.body.finKms,
+		entry.fecha =   req.body.fecha, 
+		entry.kmsRecorridos =  req.body.kmsRecorridos
+	
+		entry.save(function(err) {
 			if(err) return res.send(500, err.message);
-      res.status(200).jsonp(tvshow);
+      		res.status(200).jsonp(entry);
 		});
 	});
 };
